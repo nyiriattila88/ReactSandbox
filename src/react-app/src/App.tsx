3449dd7ce041910cfa396548c1e6b782bbc8ec34
Attila Nyiri <Attila.Nyiri@quest.com>
2023-05-31 14:39:15 +0200

    Added a simple Alert module

--- src/react-app/src/App.tsx
+++ src/react-app/src/App.tsx
@@ -1,13 +1,14 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+
 
 function App() {
-    let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-
-    const handleSelectItem = (item: string) => {
-        console.log(item);
-    }
-
- return <div> <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} /> </div>
+  return (
+    <div>
+        <Alert>
+            Hello World
+        </Alert>
+    </div>
+  );
 }
 
 export default App;
